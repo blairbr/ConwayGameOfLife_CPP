dfb2ad507a69465d14f5b8c1233aa5406dac33fb
blair.brown <blair.brown@accenture.com>
2020-01-02 14:49:37 -0500

    Testing for enough live neighbors to support life

--- src/rules.cpp
+++ src/rules.cpp
@@ -1,5 +1,10 @@
 #include "rules.h"
 
 bool applyRules(bool cellIsAlive, int numberOfNeighbors) {
+    if(numberOfNeighbors > 1)
+    {
+        return true;
+    }
     return false;
+
 }
--- test/testdefault.cpp
+++ test/testdefault.cpp
@@ -9,3 +9,9 @@ TEST(neighbors, liveCellWithFewerThanTwoLiveNeighborsDies) {
 
     EXPECT_EQ(applyRules(cellIsAlive, neighborCount), false);
 }
+TEST(neighbors, liveCellWithTwoOrThreeLiveNeighborsLives) {
+    bool cellIsAlive = true;
+    int neighborCount = 2;
+
+    EXPECT_EQ(applyRules(cellIsAlive, neighborCount), true);
+}
